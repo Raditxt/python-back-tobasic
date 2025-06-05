891a8671d89e2976da6dcc388f2b921e49df32f7
unknown <radityamulyaakbar2@gmail.com>
2025-06-05 07:45:18 -0500

    first

---
+++ Basic/Variables.py
@@ -0,0 +1,24 @@
+#Variable
+x = "10"
+y =" 5"
+print(x)
+print(y)
+
+#casting
+x =  str(3)
+y = float(4.5)
+print(x)
+print(y)
+
+#Single or Double Quotes
+# Both single and double quotes can be used for strings in Python
+x = "Johne"
+x = 'Johnt'
+print(x) #Yang terakhir yang akan disimpan
+
+#Case Sensitive
+#Case Sensitive biasanya digunakan untuk membedakan variabel yang berbeda walaupun hurufnya sama tapi yang membedakan ialah huruf kapital dan kecil
+a = "Sally"
+A = "Sally"
+print(a)
+print(A) #Python is case sensitive, so 'a' and 'A' are different variables
