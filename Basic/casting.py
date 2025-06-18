e842bc0d0a905ab239378952f7ca731564784204
unknown <radityamulyaakbar2@gmail.com>
2025-06-17 22:42:57 -0500

    Python casting

---
+++ Basic/casting.py
@@ -0,0 +1,9 @@
+#Tyoe Casting
+# Casting is the process of converting one data type to another.
+# This is often necessary when you want to perform operations that require specific data types.
+# 1. Int - Change integer to float or string
+# from float
+x = int(3.14)
+y = int(5.99)
+
+# from string
