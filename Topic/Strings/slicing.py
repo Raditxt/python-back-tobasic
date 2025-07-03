5e34bcc101d55d6610885e118980d53e618aa543
unknown <radityamulyaakbar2@gmail.com>
2025-07-03 03:17:05 -0500

    slicing

---
+++ Topic/Strings/slicing.py
@@ -0,0 +1,15 @@
+#Slicing 
+name = "Rin"
+print(name[0]) #R
+print(name[1]) #i
+print(name[2]) #n
+
+#Slicing with start index and end index
+# take character from index 0 to 3
+name = "Rin"
+print(name[0:3]) #Rin
+# take character from index 0 to 2
+name = "Rin"
+print(name[0:2]) #R
+
+#S
